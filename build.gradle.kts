93e8f8bdd421488ee5d6af22906a4e9e8fe7d37c
ntnang <ntnang91@gmail.com>
2024-05-23 17:48:56 +0700

    v1.0.8: intellij build 241 compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.example"
-version = "1.0.7"
+version = "1.0.9"
 
 repositories {
     mavenCentral()
@@ -18,7 +18,7 @@ java {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2023.3.2")
+    version.set("2024.1.1")
     type.set("IC") // Target IDE Platform
 
     plugins.set(listOf(/* Plugin Dependencies */))
@@ -36,7 +36,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("222")
-        untilBuild.set("233.*")
+        untilBuild.set("241.*")
     }
 
     signPlugin {
